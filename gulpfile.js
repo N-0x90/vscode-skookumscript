682c05aa68de9394d07f7056d2aee9b21f864571
N0x90 <cervantes.nestor@gmail.com>
2019-09-23 09:14:57 -0500

    update gulpfile simple form, remove cs json

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,10 @@ const yaml = require('gulp-yaml');
 const inputGrammar = "syntaxes/skookumscript.tmLanguage.yaml";
 const grammarsDirectory = "syntaxes/";
 
-gulp.task('buildTmLanguage', done => {
+gulp.task('default', done => {
     gulp.src(inputGrammar)
       .pipe(yaml({ schema: 'DEFAULT_SAFE_SCHEMA' }))
       .pipe(gulp.dest(grammarsDirectory))
 
     done();
 });
-
-gulp.task('default', gulp.series(gulp.parallel('buildTmLanguage')));
